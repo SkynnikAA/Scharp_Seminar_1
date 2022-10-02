071c3113b69956951a55d87edb6c9b9128d9b6be
Skrynnik Aleksander <Devid99@yandex.ru>
2022-10-02 22:05:41 +0500

    Решение  ДЗ Семинар 1 задача №4

---
+++ Z4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+int max = 0;
+Console.WriteLine("Ведите a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > max) max = a ;
+if(b > max) max = b ;
+if(c > max) max = c ;
+
+Console.WriteLine($"max = {max}");
