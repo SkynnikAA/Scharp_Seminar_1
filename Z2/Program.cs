2ea50daeb672239d6035ee1a328d184ab2ff1019
Skrynnik Aleksander <Devid99@yandex.ru>
2022-10-02 21:40:50 +0500

    Решение  ДЗ Семинар 1 задача №2 + описание

--- Z2/Program.cs
+++ Z2/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Clear();
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
 Console.WriteLine("Ведите a:");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Ведите b:");
