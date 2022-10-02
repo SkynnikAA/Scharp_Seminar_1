fee395a3957c66cab920e50442001f3715bb3930
Skrynnik Aleksander <Devid99@yandex.ru>
2022-10-02 21:27:40 +0500

    Решение  ДЗ Семинар 1 задача №2

---
+++ Z2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Ведите a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"max a = {a}");
+}
+else
+{
+    Console.WriteLine($"max b = {b}");
+}
